56a3a0e8aa4e4d5ad9623e91cc9d1a543e26aa68
ImCaO <imcao@imcao.cn>
2021-11-09 10:29:12 +0800

    允许跨域请求

---
+++ src/main/java/cn/imcao/influxdbapi/config/GlobalCorsConfig.java
@@ -0,0 +1,38 @@
+package cn.imcao.influxdbapi.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Slf4j
+public class GlobalCorsConfig {
+
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        log.info(this.allowedOrigin);
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin(allowedOrigin);
+        config.setAllowCredentials(true);
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(configSource);
+    }
+}
