e1a434a9f7d0f6dc294b4be940f7902f1edfd793
ImCaO <imcao@imcao.cn>
2021-12-15 21:11:34 +0800

    encryption

--- src/test/java/cn/imcao/influxdbapi/InfluxDbApiApplicationTests.java
+++ src/test/java/cn/imcao/influxdbapi/InfluxDbApiApplicationTests.java
@@ -1,13 +1,26 @@
 package cn.imcao.influxdbapi;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InfluxDbApiApplicationTests {
 
+    @Autowired
+    private StringEncryptor stringEncryptor;
+
     @Test
     void contextLoads() {
+    }
+
+    private String encrypt(String originPassword) {
+        return stringEncryptor.encrypt(originPassword);
+    }
+
+    private String decrypt(String encryptedPassword) {
+        return stringEncryptor.decrypt(encryptedPassword);
     }
 
 }
